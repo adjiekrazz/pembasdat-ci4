f5237854bd40cfde041f6d0eb298294c1b82fe50
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2021-06-06 17:06:14 +0700

    add products page

---
+++ app/Views/products.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Products - Codeigniter 4</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+
+	<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>">
+</head>
+<body>
+    <?= view('_partials/navbar'); ?>
+
+    <div class="container mt-4">
+        <div class="card">
+            <div class="card-body">
+                Welcome to products page.
+            </div>
+        </div>
+    </div>
+
+<!-- Bootstrap JS -->
+<script src="<?= base_url('js/bootstrap.bundle.min.js') ?>"></script>
+</body>
+</html>
