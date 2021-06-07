7c8ed3cebbeb428afcbf37d2528ddc5640c23b53
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2021-06-07 10:31:05 +0700

    change dashboard title

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Krazz Codeigniter 4</title>
+	<title>Dashboard - Codeigniter 4</title>
 	<meta name="description" content="The small framework with powerful features">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
