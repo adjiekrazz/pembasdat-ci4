e836722f22308d98ea319f17a17bd6f29f78bfc0
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2021-06-14 00:41:04 +0700

    add the customized myth-auth routes with published AuthController

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,6 +35,30 @@ $routes->setAutoRoute(true);
 $routes->get('/', 'Dashboard::index');
 
 /*
+ * Myth:Auth routes file.
+ */
+$routes->group('', ['namespace' => 'App\Controllers'], function ($routes) {
+    // Login/out
+    $routes->get('login', 'AuthController::login', ['as' => 'login']);
+    $routes->post('login', 'AuthController::attemptLogin');
+    $routes->get('logout', 'AuthController::logout');
+
+    // Registration
+    $routes->get('register', 'AuthController::register', ['as' => 'register']);
+    $routes->post('register', 'AuthController::attemptRegister');
+
+    // Activation
+    $routes->get('activate-account', 'AuthController::activateAccount', ['as' => 'activate-account']);
+    $routes->get('resend-activate-account', 'AuthController::resendActivateAccount', ['as' => 'resend-activate-account']);
+
+    // Forgot/Resets
+    $routes->get('forgot', 'AuthController::forgotPassword', ['as' => 'forgot']);
+    $routes->post('forgot', 'AuthController::attemptForgot');
+    $routes->get('reset-password', 'AuthController::resetPassword', ['as' => 'reset-password']);
+    $routes->post('reset-password', 'AuthController::attemptReset');
+});
+
+/*
  * --------------------------------------------------------------------
  * Additional Routing
  * --------------------------------------------------------------------
