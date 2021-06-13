7c8af23324cbba7c19faf46f0cc6015587cdaf92
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2021-06-14 00:47:23 +0700

    add the main seeder that calls all seeders

---
+++ app/Database/Seeds/DatabaseSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+	public function run()
+	{
+		$this->call('UserSeeder');
+        $this->call('ProductSeeder');
+	}
+}
