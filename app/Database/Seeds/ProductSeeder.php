35de78b015605d7d757d151e0f07c3116a777211
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2021-07-03 14:57:44 +0700

    add more products on productSeeder

--- app/Database/Seeds/ProductSeeder.php
+++ app/Database/Seeds/ProductSeeder.php
@@ -21,6 +21,34 @@ class ProductSeeder extends Seeder
                 'product_name' => 'Gudang Garam Signature',
                 'product_price' => '17500'
             ],
+            [
+                'product_name' => 'Susu Dancow',
+                'product_price' => '4000'
+            ],
+            [
+                'product_name' => 'Cornetto Ice Cream',
+                'product_price' => '7000'
+            ],
+            [
+                'product_name' => 'Magnum Ice Cream',
+                'product_price' => '10000'
+            ],
+            [
+                'product_name' => 'Chocolatos',
+                'product_price' => '1000'
+            ],
+            [
+                'product_name' => 'Lays',
+                'product_price' => '5000'
+            ],
+            [
+                'product_name' => 'Kacang Garuda',
+                'product_price' => '4000'
+            ],
+            [
+                'product_name' => 'Big Cola',
+                'product_price' => '5000'
+            ],
         ];
 
         $this->db->table('products')->insertBatch($data);
