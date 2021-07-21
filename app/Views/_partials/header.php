f80d4f7bf856b2ec4188a7a25d1178aea5f7fa8a
Arif Purnomo Aji <arif.purnomo@lidahbuayagroup.com>
2021-07-21 21:08:04 +0700

    fix uri_string in title for dashboard pages

--- app/Views/_partials/header.php
+++ app/Views/_partials/header.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>CMS - <?= uri_string() ?></title>
+<title>CMS - <?php echo uri_string() !== '/' ? uri_string() : 'Dashboard'; ?></title>
 
 <!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
