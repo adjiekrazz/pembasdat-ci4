b9df8334d5e3e473b777fb5145153d5aeaae5d09
Arif Purnomo Aji <arif.purnomo@lidahbuayagroup.com>
2021-07-24 08:28:43 +0700

    update version

--- app/Views/_partials/footer.php
+++ app/Views/_partials/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 2.1
+      <b>Version</b> 2.2
     </div>
     <strong>Copyright &copy; 2014-2021 <a href="http://github.com/adjiekrazz">Adjiekrazz</a>&nbsp;<a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
 </footer>
