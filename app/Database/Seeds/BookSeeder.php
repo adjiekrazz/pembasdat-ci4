c1ddba23aee30e7c4ad6a4afce291ec317895ba8
Arif Purnomo Aji <arif.purnomo@lidahbuayagroup.com>
2021-07-24 08:01:56 +0700

    add more books to seeder

--- app/Database/Seeds/BookSeeder.php
+++ app/Database/Seeds/BookSeeder.php
@@ -16,7 +16,7 @@ class BookSeeder extends Seeder
                 'author' => 'Christos Gage',
                 'publisher' => 'Penguin Random House',
                 'publication_year' => 2021,
-                'status' => 'not available'
+                'status' => 'Not Available'
             ],
             [
                 'code' => 'KD002',
@@ -25,7 +25,7 @@ class BookSeeder extends Seeder
                 'author' => 'Danielle Steel',
                 'publisher' => 'Penguin Random House',
                 'publication_year' => 2021,
-                'status' => 'available'
+                'status' => 'Available'
             ],
             [
                 'code' => 'KD003',
@@ -34,7 +34,7 @@ class BookSeeder extends Seeder
                 'author' => 'Elisabeth Lim',
                 'publisher' => 'Penguin Random House',
                 'publication_year' => 2021,
-                'status' => 'available'
+                'status' => 'Available'
             ],
             [
                 'code' => 'KD004',
@@ -43,7 +43,7 @@ class BookSeeder extends Seeder
                 'author' => 'Joyce Maynard',
                 'publisher' => 'HarperCollins',
                 'publication_year' => 2021,
-                'status' => 'available'
+                'status' => 'Available'
             ],
             [
                 'code' => 'KD005',
@@ -52,7 +52,151 @@ class BookSeeder extends Seeder
                 'author' => 'Vanessa Riley',
                 'publisher' => 'HarperCollins',
                 'publication_year' => 2021,
-                'status' => 'available'
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD006',
+                'title' => 'The Maidens',
+                'year' => 2021,
+                'author' => 'Alex Michaelides',
+                'publisher' => 'Macmillan',
+                'publication_year' => 2021,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD007',
+                'title' => 'One Last Stop',
+                'year' => 2021,
+                'author' => 'Casey McQuiston',
+                'publisher' => 'Macmillan',
+                'publication_year' => 2021,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD008',
+                'title' => 'Any Way the Wind Blows',
+                'year' => 2021,
+                'author' => 'Rainbow Rowell',
+                'publisher' => 'Macmillan',
+                'publication_year' => 2021,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD009',
+                'title' => 'American Marxism',
+                'year' => 2021,
+                'author' => 'Mark R. Levin',
+                'publisher' => 'Simon & Schuster',
+                'publication_year' => 2021,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD010',
+                'title' => 'It Ends with Us',
+                'year' => 2016,
+                'author' => 'Colleen Hoover',
+                'publisher' => 'Simon & Schuster',
+                'publication_year' => 2016,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD011',
+                'title' => 'Falling',
+                'year' => 2021,
+                'author' => 'T. J. Newman',
+                'publisher' => 'Simon & Schuster',
+                'publication_year' => 2021,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD012',
+                'title' => 'Anxious People',
+                'year' => 2021,
+                'author' => 'Fredrik Backman',
+                'publisher' => 'Simon & Schuster',
+                'publication_year' => 2021,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD013',
+                'title' => 'The Unhoneymooners',
+                'year' => 2019,
+                'author' => 'Christina Lauren',
+                'publisher' => 'Simon & Schuster',
+                'publication_year' => 2019,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD014',
+                'title' => 'Grit',
+                'year' => 2018,
+                'author' => 'Angela Duckworth',
+                'publisher' => 'Simon & Schuster',
+                'publication_year' => 2018,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD015',
+                'title' => 'Mis 10th Edition',
+                'year' => 2021,
+                'author' => 'Hossein Bidgoli',
+                'publisher' => 'Cengage',
+                'publication_year' => 2021,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD016',
+                'title' => 'Oracle 12C 3rd Edition',
+                'year' => 2016,
+                'author' => 'Joan Casteel',
+                'publisher' => 'Cengage',
+                'publication_year' => 2016,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD017',
+                'title' => 'C# Programming 5th Edition',
+                'year' => 2016,
+                'author' => 'Barbara Doyle',
+                'publisher' => 'Cengage',
+                'publication_year' => 2016,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD018',
+                'title' => 'Java Programming 8th Edition',
+                'year' => 2016,
+                'author' => 'Joyce Farrell',
+                'publisher' => 'Cengage',
+                'publication_year' => 2016,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD019',
+                'title' => 'Encyclopedia of Neuroscience',
+                'year' => 2009,
+                'author' => 'Marc D. Binder',
+                'publisher' => 'Springer Nature',
+                'publication_year' => 2009,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD020',
+                'title' => 'Successful Emotions',
+                'year' => 2016,
+                'author' => 'Lochner, Katharina',
+                'publisher' => 'Springer Nature',
+                'publication_year' => 2016,
+                'status' => 'Available'
+            ],
+            [
+                'code' => 'KD021',
+                'title' => 'Process Mining',
+                'year' => 2016,
+                'author' => 'Van Der Aalst, Wil',
+                'publisher' => 'Springer Nature',
+                'publication_year' => 2016,
+                'status' => 'Available'
             ],
         ];
 
